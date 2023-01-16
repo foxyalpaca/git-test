2548d566319682636800b0ddf36084230a3e6c40
frank.vilarino <frank.vilarino@gmail.com>
2023-01-16 13:23:54 -0500

    This is an initial commit test

---
+++ test.js
@@ -0,0 +1,13 @@
+
+
+function printArray(number) {
+  var newArray = [];
+
+  for (var i = 1; i <= number; i++) {
+    newArray.push(i);
+  }
+  console.log(newArray)
+  return newArray;
+}
+
+printArray(5);
